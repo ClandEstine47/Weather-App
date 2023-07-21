7580f018c6cf79c63c208ec0cd2ca6a658b49bb0
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2023-07-21 08:59:26 +0545

    WeatherMappers update

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,7 +1,9 @@
 package com.plcoding.weatherapp.data.mappers
 
 import com.plcoding.weatherapp.data.remote.WeatherDataDto
+import com.plcoding.weatherapp.data.remote.WeatherDto
 import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -12,7 +14,7 @@ private data class IndexedWeatherData(
 )
 
 fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
-    return time.mapIndexed{ index, time ->
+    return time.mapIndexed { index, time ->
         val temperature = temperatures[index]
         val weatherCode = weatherCode[index]
         val windSpeed = windSpeeds[index]
@@ -34,4 +36,26 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
     }.mapValues {
         it.value.map { it.data }
     }
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = if (now.minute < 30) {
+        weatherDataMap[0]?.find {
+            it.time.hour == now.hour
+        }
+    } else if (now.hour + 1 == 0) {
+        weatherDataMap[1]?.find {
+            it.time.hour == now.hour + 1
+        }
+    } else {
+        weatherDataMap[0]?.find {
+            it.time.hour == now.hour + 1
+        }
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
 }
